e7de7b1a42bf3d7be8e51f15157b04c07666d194
Liemnht <liemnht@imt-soft.com>
2019-01-21 09:06:13 +0700

    Add LoginUIViewController

---
+++ InternTraining/InternTraining/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  InternTraining
+//
+//  Created by Nguyen Hung Thanh Liem on 1/21/19.
+//  Copyright © 2019 Cong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
