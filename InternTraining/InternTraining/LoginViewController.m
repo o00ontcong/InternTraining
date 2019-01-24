36a627f761f15e2480043a8eed593d40b7609022
Liemnht <liemnht@imt-soft.com>
2019-01-24 08:47:58 +0700

    edited Main.storyboard added Screenshoot

--- InternTraining/InternTraining/LoginViewController.m
+++ InternTraining/InternTraining/LoginViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self addBottomLineToUITextField];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +33,43 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void) addBottomLineToUITextField{
+     [self addLineToView:self.emailTextField atPosition:LINE_POSITION_BOTTOM withColor:[UIColor colorWithRed:0.42 green:0.33 blue:0.76 alpha:1.0] lineWitdh:1];
+    [self addLineToView:self.passwordTextField atPosition:LINE_POSITION_BOTTOM withColor:[UIColor colorWithRed:0.42 green:0.33 blue:0.76 alpha:1.0] lineWitdh:1];
+    
+    
+}
+
+//UITextField must contain an auto layout to use this method
+typedef enum : NSUInteger {
+    LINE_POSITION_TOP,
+    LINE_POSITION_BOTTOM
+} LINE_POSITION;
+
+- (void) addLineToView:(UIView *)view atPosition:(LINE_POSITION)position withColor:(UIColor *)color lineWitdh:(CGFloat)width {
+    // Add line
+    UIView *lineView = [[UIView alloc] init];
+    [lineView setBackgroundColor:color];
+    [lineView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [view addSubview:lineView];
+    
+    NSDictionary *metrics = @{@"width" : [NSNumber numberWithFloat:width]};
+    NSDictionary *views = @{@"lineView" : lineView};
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[lineView]|" options: 0 metrics:metrics views:views]];
+    
+    switch (position) {
+        case LINE_POSITION_TOP:
+            [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[lineView(width)]" options: 0 metrics:metrics views:views]];
+            break;
+            
+        case LINE_POSITION_BOTTOM:
+            [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[lineView(width)]|" options: 0 metrics:metrics views:views]];
+            break;
+        default: break;
+    }
+}
+
+
 
 @end
