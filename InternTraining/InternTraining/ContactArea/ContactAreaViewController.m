92c9137718977f4176d7e00291e8b0f1480746af
Liemnht <liemnht@imt-soft.com>
2019-03-08 11:22:25 +0700

    add query .sql truyền vào shopid lấy ds sản phẩm có giá

--- InternTraining/InternTraining/ContactArea/ContactAreaViewController.m
+++ InternTraining/InternTraining/ContactArea/ContactAreaViewController.m
@@ -43,6 +43,10 @@
     [self getDistrictListWithProvinceAREA_ID:provinceID];
     NSInteger districtId = [[DistrictArea_ID objectAtIndex:0]integerValue];
     [self getPrecinctListWithDistrictAREA_ID:districtId];
+    
+    
+    [self getDistrictWithBiggestAreaCodeOfProvince];
+    [self getProcductPriceWithShopid];
 }
 
 //MARK:Table DataSource
@@ -83,7 +87,9 @@
         FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
         [database open];
 
-        NSString *sqlSelectQuery = @"SELECT Count(PROVINCE) as count FROM AREA where PARENT_AREA_ID = 2";
+        NSString *sqlSelectQuery = @"SELECT Count(PROVINCE) as count "
+                                    "FROM AREA "
+                                    "where PARENT_AREA_ID = 2";
         
         // Query result
         FMResultSet *result = [database executeQuery:sqlSelectQuery];
@@ -105,7 +111,9 @@
     [database open];
     
     //Query result
-    NSString *sqlSelectQuery = @"SELECT PROVINCE_NAME,AREA_ID FROM AREA where PARENT_AREA_ID = 2 ";
+    NSString *sqlSelectQuery = @"SELECT PROVINCE_NAME,AREA_ID "
+                                "FROM AREA "
+                                "where PARENT_AREA_ID = 2 ";
      FMResultSet *result = [database executeQuery:sqlSelectQuery];
     if (![result next]) {
         NSLog(@"Error: No data Found");
@@ -130,7 +138,9 @@
     [database open];
     
     //Query result
-    NSString *sqlSelectQuery = [NSString stringWithFormat:@"SELECT AREA_ID,DISTRICT_NAME FROM AREA where PARENT_AREA_ID = %ld",provinceAREA_ID];
+    NSString *sqlSelectQuery = [NSString stringWithFormat:@"SELECT AREA_ID,DISTRICT_NAME "
+                                                            "FROM AREA "
+                                                            "where PARENT_AREA_ID = %ld",provinceAREA_ID];
     
     FMResultSet *result = [database executeQuery:sqlSelectQuery];
     if (![result next]) {
@@ -175,6 +185,83 @@
     [database close];
 }
 
+//MARK: Query Area
+
+-(void)getDistrictWithBiggestAreaCodeOfProvince{
+    NSArray  *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsPath = [paths objectAtIndex:0];
+    NSString *dbPath = [docsPath stringByAppendingPathComponent:@"1051_2880_DMSCoreDatabase.sqlite"];
+    FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
+    [database open];
+    
+    //Query result
+    NSString *sqlSelectQuery = [NSString stringWithFormat:@"SELECT PARENT_AREA_ID, AREA_ID, PROVINCE_NAME, "
+                                "DISTRICT_NAME, Max(AREA_CODE) as AREA_CODE "
+                                "from AREA WHERE PROVINCE in "
+                                "(SELECT PROVINCE FROM AREA where PARENT_AREA_ID = 2) "
+                                "and PRECINCT_NAME is NULL "
+                                "and DISTRICT_NAME is not NULL "
+                                "GROUP by PROVINCE_NAME "
+                                "ORDER by PARENT_AREA_ID"];
+    
+    FMResultSet *result = [database executeQuery:sqlSelectQuery];
+    if (![result next]) {
+        NSLog(@"Error: No data Found");
+    }
+    else{
+        do{
+            NSString *PARENT_AREA_ID = [NSString  stringWithFormat:@"%d",[result intForColumn:@"PARENT_AREA_ID"]];
+            NSString *PROVINCE_NAME = [result stringForColumn:@"PROVINCE_NAME"];
+            NSString *DISTRICT_NAME = [result stringForColumn:@"DISTRICT_NAME"];
+            NSString *AREA_ID = [NSString  stringWithFormat:@"%d",[result intForColumn:@"AREA_ID"]];
+            NSString *AREA_CODE = [result stringForColumn:@"AREA_CODE"];
+            //Log query result
+            NSLog(@"ParentAreaId = %@, ProvinceName = %@, DistrictName = %@, AreaID = %@, AreaCode = %@"
+                  ,PARENT_AREA_ID,PROVINCE_NAME,DISTRICT_NAME,AREA_ID,AREA_CODE);
+        }while ([result next]);
+    }
+    [database close];
+}
+
+
+//MARK: Query Shop-Product-Price
+-(void)getProcductPriceWithShopid{
+    NSArray  *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsPath = [paths objectAtIndex:0];
+    NSString *dbPath = [docsPath stringByAppendingPathComponent:@"1051_2880_DMSCoreDatabase.sqlite"];
+    FMDatabase *database = [FMDatabase databaseWithPath:dbPath];
+    [database open];
+    
+    //Query result
+    NSString *sqlSelectQuery = [NSString stringWithFormat:@"SELECT PARENT_AREA_ID, AREA_ID, PROVINCE_NAME, "
+                                "DISTRICT_NAME, Max(AREA_CODE) as AREA_CODE "
+                                "from AREA WHERE PROVINCE in "
+                                "(SELECT PROVINCE FROM AREA where PARENT_AREA_ID = 2) "
+                                "and PRECINCT_NAME is NULL "
+                                "and DISTRICT_NAME is not NULL "
+                                "GROUP by PROVINCE_NAME "
+                                "ORDER by PARENT_AREA_ID"];
+    
+    FMResultSet *result = [database executeQuery:sqlSelectQuery];
+    if (![result next]) {
+        NSLog(@"Error: No data Found");
+    }
+    else{
+        NSLog(@"ProcductPrice: %@",[result resultDictionary]);
+        
+//        do{
+//            NSString *PARENT_AREA_ID = [NSString  stringWithFormat:@"%d",[result intForColumn:@"PARENT_AREA_ID"]];
+//            NSString *PROVINCE_NAME = [result stringForColumn:@"PROVINCE_NAME"];
+//            NSString *DISTRICT_NAME = [result stringForColumn:@"DISTRICT_NAME"];
+//            NSString *AREA_ID = [NSString  stringWithFormat:@"%d",[result intForColumn:@"AREA_ID"]];
+//            NSString *AREA_CODE = [result stringForColumn:@"AREA_CODE"];
+//            //Log query result
+//            NSLog(@"ParentAreaId = %@, ProvinceName = %@, DistrictName = %@, AreaID = %@, AreaCode = %@"
+//                  ,PARENT_AREA_ID,PROVINCE_NAME,DISTRICT_NAME,AREA_ID,AREA_CODE);
+//        }while ([result next]);
+    }
+    [database close];
+}
 
 
 //MARK: Dropdown Touch action
